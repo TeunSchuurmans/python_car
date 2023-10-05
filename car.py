e58b5817e358faafae29458dfaa04402d826e549
Teun-Schuurmans <ps241132@summacollege.nl>
2023-10-05 11:22:02 +0200

    further worked on car mechanics

--- car.py
+++ car.py
@@ -5,36 +5,64 @@ from settings import *
 
 
 class Car:
-    max_speed = MAX_SPEED                       #defining the class constants
+    max_speed = MAX_SPEED   #defining the class constants
     acceleration = ACCELERATION 
     friction = FRICTION
-    rotation_speed = ROTATION_SPEED
+    car_width = 25
+    car_height = 50
+    car_image = pg.transform.rotate(pg.transform.scale(pg.image.load('images/car.png'), (car_height, car_width)), 90)
 
-    def __init__(self):
+
+    def __init__(self, game):
+        
+        self.game = game
         self.x, self.y = CAR_POSITION
         self.angle = 0
         self.speed = 0
+        self.rotation_speed = ROTATION_SPEED
 
     def movement(self):
 
-        dx, dy = 0, 0                           #delta x and delta y
+        dx, dy = 0, 0   #delta x and delta y
+
+        #self.rotation_speed = (self.speed/13)/self.friction
 
         input = pg.key.get_pressed()
 
-        if input[pg.K_w]:                       #if the W key is pressed, the speed will increment by the acceleration
-            if self.speed < Car.max_speed:      #makes sure the speed can't be higher than the max speed
-                self.speed += Car.acceleration
-        else:                                   #if the W key is not pressed, the speed will decrement by the acceleration
-            if self.speed > 0:                  #makes sure the car's speed is higher than 0
-                self.speed -= Car.acceleration
+        if input[pg.K_w]:   #if the W key is pressed, the speed will increment by the acceleration
+            self.speed = min(self.speed + Car.acceleration, Car.max_speed)
+        else:               #if the W key is not pressed, the speed will decrement by the friction
+            self.speed = max(self.speed - Car.friction, 0)
         
         if input[pg.K_a]:
-            self.angle -= Car.rotation_speed    #rotates the car to the left
+            self.angle += self.rotation_speed *10    #rotates the car to the left
+            
         if input[pg.K_d]:
-            self.angle += Car.rotation_speed    #rotates the car to the right
+            self.angle -= self.rotation_speed *10    #rotates the car to the right
+            
 
-        self.x += dx                            #adds the X increment to the car's X position                       
-        self.y += dy                            #adds the Y increment to the car's Y position
+        radian = math.radians(self.angle)                  #converts the angle into a radiant
+
+        dx = math.sin(radian) * self.speed
+        dy = math.cos(radian) * self.speed
+
+        self.x -= dx    #adds the X increment to the car's X position                       
+        self.y -= dy    #adds the Y increment to the car's Y position
+
         
+
+    
+    def draw(self): 
+        center_X = self.x + (Car.car_width / 2)
+        center_Y = self.y + (Car.car_height / 2)
+
+        rotated_image = pg.transform.rotate(Car.car_image, self.angle)
+        
+        self.game.screen.blit(rotated_image, (self.x, self.y))
+
+        car_center = pg.draw.circle(self.game.screen, 'white', (center_X, center_Y), 2)
+        
+        
+
     def update(self):
         self.movement()
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame as pg
 import sys
 from settings import *
+from car import *
 
 
 class Game:
@@ -8,18 +9,24 @@ class Game:
         pg.init()   #initializes pygame
         self.screen = pg.display.set_mode(RES)  #sets the screen resolution
         self.clock = pg.time.Clock()    #creates an instance of Clock()
+        self.new_game()
+
+    def new_game(self):
+        self.car = Car(self)
 
     def update(self):
+        self.car.update()
         pg.display.flip()   #updates the screen
         self.clock.tick(FPS)    # adds a delay
         pg.display.set_caption(f'{self.clock.get_fps() :.1f}')
 
     def draw(self):
         self.screen.fill('black')   #draws the screen black
+        self.car.draw()
 
     def check_events(self): #listens for events
         for event in pg.event.get():
-            if event.type == pg.QUIT:
+            if event.type == pg.QUIT or (event.type == pg.KEYUP and event.key == pg.K_ESCAPE):
                 pg.quit()   #stops pygame
                 sys.exit()  #stops the program
 
