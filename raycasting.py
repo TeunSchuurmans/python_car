60c81f5fa878e1fc6f10df655ecbc5f2e76b5272
Teun-Schuurmans <ps241132@summacollege.nl>
2023-10-28 19:13:55 +0200

    added ray_angle function to raycasting.py

--- raycasting.py
+++ raycasting.py
@@ -8,15 +8,19 @@ class RayCaster:
         self.car = car
         self.num_rays = NUM_OF_RAYS
         self.car_fov = CAR_FOV
+        self.half_fov = self.car_fov / 2
+        self.ray_gap = self.car_fov / self.num_rays
         self.ray_ends = [_ for _ in range(self.num_rays)]
 
-    @property
-    def ray_gap(self):
-        return self.car_fov / self.num_rays
-    
+    #utility functions
+    def ray_angle(self, x):
+        return math.radians((self.car.angle - self.half_fov) + self.ray_gap * x) 
+
+
+    #loop functions
     def cast_rays(self):
         for x in range(self.num_rays):
-            self.ray_ends[x] = (self.car.car_center_x + math.sin(self.car.radians) * -100, self.car.car_center_y + math.cos(self.car.radians) * -100)
+            self.ray_ends[x] = (self.car.car_center_x + math.sin(self.ray_angle(x)) * -100, self.car.car_center_y + math.cos(self.ray_angle(x)) * -100)
 
     def draw(self):
         for x in self.ray_ends:
