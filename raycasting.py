b986d0f806488244b93dcf82443af3267fcd9da3
Teun-Schuurmans <ps241132@summacollege.nl>
2023-10-31 13:22:13 +0100

    changed some variables

--- raycasting.py
+++ raycasting.py
@@ -9,7 +9,7 @@ class RayCaster:
         self.car = car
         self.car_fov = CAR_FOV
         self.half_fov = self.car_fov / 2
-        self.ray_gap = self.car_fov / (NUM_OF_RAYS - 1)
+        self.ray_gap = self.car_fov / (NUM_OF_RAYS )
         self.ray_ends = [_ for _ in range(NUM_OF_RAYS)]
 
     #utility functions
@@ -17,6 +17,10 @@ class RayCaster:
     def start_point(self):
         return (self.car.car_center_x, self.car.car_center_y)
 
+    @property
+    def tile_pos(self):
+        return (self.car.current_tile[0] * TILE_SIZE, self.car.current_tile[1] * TILE_SIZE)
+    
     def ray_angle(self, x):
         return math.radians(self.car.angle - self.half_fov + self.ray_gap * x)
 
@@ -29,21 +33,23 @@ class RayCaster:
         for x, _ in enumerate(self.ray_ends):
             
             #variables for horizontal raycasting
-            hor_d = (TILE_SIZE,(TILE_SIZE / (math.tan(self.ray_angle(x)) + -1e6)))
-            hor_x_end= self.car.current_tile[0] * TILE_SIZE
-            hor_y_end = self.car.current_tile[1] * TILE_SIZE
+            hor_dx = TILE_SIZE
+            hor_dy = TILE_SIZE / (math.tan(self.ray_angle(x)) + -1e6)
+            hor_x_end= self.tile_pos[0]
+            hor_y_end = self.tile_pos[1]
 
             #variables for vertical raycasting
-            ver_d = ((math.tan(self.ray_angle(x)) * TILE_SIZE),TILE_SIZE)
-            ver_x_end = self.car.current_tile[0] * TILE_SIZE
-            ver_y_end = self.car.current_tile[1] * TILE_SIZE
+            ver_dx = math.tan(self.ray_angle(x)) * TILE_SIZE
+            ver_dy = TILE_SIZE
+            ver_x_end = self.tile_pos[0]
+            ver_y_end = self.tile_pos[1]
             
             #horizontal
             for _ in self.road.tile_map[0]:
                 if 1 == 2:
                     break
-                hor_x_end += hor_d[0]
-                hor_y_end += hor_d[1]
+                hor_x_end += hor_dx
+                hor_y_end += hor_dy
                 pg.draw.circle(self.game.screen,
                 'orange',
                 (hor_x_end, hor_y_end),
@@ -53,16 +59,14 @@ class RayCaster:
             for _ in self.road.tile_map:
                 if 1 == 2:
                         break
-                ver_x_end += ver_d[0]
-                ver_y_end += ver_d[1]
+                ver_x_end += ver_dx
+                ver_y_end += ver_dy
                 pg.draw.circle(self.game.screen,
                 'orange',
                 (ver_x_end, ver_y_end),
                 4)
-            
-            #collision_point = min(hor_collision, ver_collision)
-
-            #self.ray_ends[x] = collision_point
+    
+    
             self.ray_ends[x] = (self.start_point[0] + math.sin(self.ray_angle(x)) * -100, self.start_point[1] + math.cos(self.ray_angle(x)) * -100)
 
     def draw(self):
