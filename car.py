69f3b286385b940baf377a1477e24d0c00860184
Teun-Schuurmans <ps241132@summacollege.nl>
2023-11-07 10:53:52 +0100

    cleaned up code in raycasting.py and car.py

--- car.py
+++ car.py
@@ -1,30 +1,19 @@
 import pygame as pg
 import math
 from settings import *
-from raycasting import * 
-
-
+from raycasting import *
 
 class Car:
-    max_speed = MAX_SPEED
-    acceleration = ACCELERATION 
-    friction = FRICTION
-    rotation_speed = ROTATION_SPEED
-    cornering_speed = CORNERING_SPEED
-    car_width = CAR_WIDTH
-    car_height = CAR_HEIGHT
-
-
-    def __init__(self, game, road): 
+    def __init__(self, game, road):
         self.game = game
         self.road = road
         self.raycaster = RayCaster(self.game, self.road, self)
-        self.car = pg.transform.rotate(pg.transform.scale(pg.image.load(CAR_IMAGE), (Car.car_height, Car.car_width)), 90)
+        self.car = pg.transform.rotate(pg.transform.scale(pg.image.load(CAR_IMAGE), (CAR_HEIGHT, CAR_WIDTH)), 90)
         self.x, self.y = road.start_pos
         self.angle = 0
         self.speed = 0
-        self.rotation_speed = 0 
-        
+        self.rotation_speed = 0
+
     #utility functions
     def in_range(self, min, max, pos):
         if max is None:
@@ -38,36 +27,30 @@ class Car:
     def radians(self):
         return math.radians(self.angle)
     
-
     #collision detection properties
     @property
     def current_tile(self):
-        return (self.car_center_x // TILE_SIZE, self.car_center_y // TILE_SIZE)
-    
-    @property
-    def car_center_x(self):
-        return self.x + (Car.car_width / 2)
+        return (self.center[0] // TILE_SIZE, self.center[1] // TILE_SIZE)
 
     @property
-    def car_center_y(self):
-        return self.y + (Car.car_height / 2)
-           
+    def center(self):
+        return (self.x + (CAR_WIDTH / 2), self.y + (CAR_HEIGHT / 2))
+
     @property
     def left_tile_border(self):
-        return self.current_tile[0] * 100
+        return self.current_tile[0] * TILE_SIZE
 
     @property
     def down_tile_border(self):
-        return self.current_tile[1] * 100 + TILE_SIZE
+        return self.current_tile[1] * TILE_SIZE + TILE_SIZE
 
     @property
     def right_tile_border(self):
-        return self.current_tile[0] * 100 + TILE_SIZE
+        return self.current_tile[0] * TILE_SIZE + TILE_SIZE
 
     @property
     def up_tile_border(self):
-        return self.current_tile[1] * 100
- 
+        return self.current_tile[1] * TILE_SIZE
 
     #loop functions
     def listen_inputs(self):
@@ -75,33 +58,32 @@ class Car:
         input = pg.key.get_pressed()
 
         #go forward
-        if input[pg.K_w]:   
-            self.speed = min(self.speed + Car.acceleration, Car.max_speed)
-        else:               
-            self.speed = max(self.speed - Car.friction, 0)
+        if input[pg.K_w]:
+            self.speed = min(self.speed + ACCELERATION, MAX_SPEED)
+        else:
+            self.speed = max(self.speed - FRICTION, 0)
 
         #rotate left
         if input[pg.K_a]:
-            self.angle += self.rotation_speed   
+            self.angle += self.rotation_speed
 
-        #rotate right 
+        #rotate right
         if input[pg.K_d]:
-            self.angle -= self.rotation_speed   
+            self.angle -= self.rotation_speed
 
     def movement(self):
-        print(self.game.delta_time)
+        #print(self.game.delta_time)
         #self.speed *= self.game.delta_time
-        dx, dy = 0, 0     
 
         if self.speed <= FRICTION:
             self.rotation_speed = 0
         else:
-            self.rotation_speed = Car.rotation_speed / (1+ (self.speed / Car.cornering_speed))
+            self.rotation_speed = ROTATION_SPEED / (1+ (self.speed / CORNERING_SPEED))
 
         dx = math.sin(self.radians) * self.speed
         dy = math.cos(self.radians) * self.speed
 
-        self.check_collision(dx, dy)    
+        self.check_collision(dx, dy)
 
     def check_collision(self, dx, dy):
 
@@ -109,54 +91,54 @@ class Car:
             match self.road.road_dict[self.current_tile[0], self.current_tile[1]]:
                 #finish
                 case 1:
-                    if self.in_range(self.left_tile_border, self.right_tile_border , self.car_center_x - dx):
+                    if self.in_range(self.left_tile_border, self.right_tile_border , self.center[0] - dx):
                         self.x -= dx
                     self.y -= dy
                 #vertical
                 case 2:
-                    if self.in_range(self.left_tile_border, self.right_tile_border , self.car_center_x - dx):
+                    if self.in_range(self.left_tile_border, self.right_tile_border , self.center[0] - dx):
                         self.x -= dx
                     self.y -= dy
                 #horizontal
-                case 3: 
-                    if self.in_range(self.up_tile_border, self.down_tile_border , self.car_center_y - dy):
+                case 3:
+                    if self.in_range(self.up_tile_border, self.down_tile_border , self.center[1] - dy):
                         self.y -= dy
                     self.x -= dx
                 #left->down
                 case 4:
-                    if self.in_range(self.up_tile_border, None, self.car_center_y - dy):
+                    if self.in_range(self.up_tile_border, None, self.center[1] - dy):
                         self.y -= dy
-                    if self.in_range(None, self.right_tile_border, self.car_center_x - dx):
+                    if self.in_range(None, self.right_tile_border, self.center[0] - dx):
                         self.x -= dx
                 #down->right
                 case 5:
-                    if self.in_range(self.up_tile_border, None, self.car_center_y - dy):
+                    if self.in_range(self.up_tile_border, None, self.center[1] - dy):
                         self.y -= dy
-                    if self.in_range(self.left_tile_border, None, self.car_center_x - dx):
+                    if self.in_range(self.left_tile_border, None, self.center[0] - dx):
                         self.x -= dx
                 #right->up
                 case 6:
-                    if self.in_range(None, self.down_tile_border, self.car_center_y - dy):
+                    if self.in_range(None, self.down_tile_border, self.center[1] - dy):
                         self.y -= dy
-                    if self.in_range(self.left_tile_border, None, self.car_center_x - dx):
+                    if self.in_range(self.left_tile_border, None, self.center[0] - dx):
                         self.x -= dx
                 #up->left
                 case 7:
-                    if self.in_range(None, self.down_tile_border, self.car_center_y - dy):
+                    if self.in_range(None, self.down_tile_border, self.center[1] - dy):
                         self.y -= dy
-                    if self.in_range(None, self.right_tile_border, self.car_center_x - dx):
+                    if self.in_range(None, self.right_tile_border, self.center[0] - dx):
                       self.x -= dx
 
-        #if a player is on grass, there is no collision               
+        #if a player is on grass, there is no collision
         else:
             self.x -= dx
             self.y -=dy
 
-    def draw(self): 
-        
+    def draw(self):
+
         #makes sure the car rotates around its center
         rotated_car = pg.transform.rotate(self.car, self.angle)
-        car_rect = rotated_car.get_rect(center=(self.car_center_x, self.car_center_y))
+        car_rect = rotated_car.get_rect(center=(self.center))
 
         self.game.screen.blit(rotated_car, car_rect.topleft)
 
--- raycasting.py
+++ raycasting.py
@@ -2,20 +2,34 @@ import pygame as pg
 import math
 from settings import *
 
+"""
+This class is used for the bot car to receive information about the distance to the wall.
+This way, the bot car can make decisions based on this information using the neural network.
+First, it calculates the horizontal and vertical depth the ray should move by.
+It does this by using the ray's angle and the tile size.
+This makes the collision detection for the ray optimal.
+Next, it checks each step for a collision with a wall.
+It does this by using the the properties from the road class.
+If a collision is detected, the cycle will break. Indicating that the sensor has come across a border.
+Then, it compares the horizontal and vertical collision point, and picks the closest one.
+This coordinate can then be used to calculate the distance from the wall to the car.
+It does this for each ray, every frame.
+"""
+
+
+#instead of starting the collision detection at the car, it starts at the point on the grid behind the car
+
+
 class RayCaster:
     def __init__(self, game, road, car):
         self.game = game
         self.road = road
         self.car = car
         self.half_fov = RAY_SPREAD / 2
-        self.ray_gap = RAY_SPREAD / (NUM_OF_RAYS - 1)
+        self.ray_gap = RAY_SPREAD / (NUM_OF_RAYS if NUM_OF_RAYS == 1 else NUM_OF_RAYS - 1)
         self.ray_ends = [_ for _ in range(NUM_OF_RAYS)]
-        print(self.ray_gap, RAY_SPREAD, math.pi)
 
     #utility functions
-    @property
-    def start_point(self):
-        return (self.car.car_center_x, self.car.car_center_y)
 
     @property
     def tile_pos(self):
@@ -33,52 +47,33 @@ class RayCaster:
 
     #loop functions
     def cast_rays(self):
-        for x, _ in enumerate(self.ray_ends):            
+        for x, _ in enumerate(self.ray_ends):          
+            #ver_len = self.check_collision()
+            #hor_len = self.check_collision()
+
+            pg.draw.line(self.game.screen, 'green', self.tile_pos, self.car.center, 3)
+            self.ray_ends[x] = (self.car.center[0] + math.sin(self.ray_angle(x)) * -100, self.car.center[1] + math.cos(self.ray_angle(x)) * -100)
+
+    def check_collision(self, dx, dy):
+        end_point = 0,0
+
+        for x in range(10):
+            pass
+        
+        return end_point
             
-            #horizontal raycasting
-            hor_dx = math.tan(self.ray_angle(x)) * TILE_SIZE
-            hor_dy = TILE_SIZE
-            hor_x_end= self.tile_pos[0]
-            hor_y_end = self.tile_pos[1]
-
-            for _ in self.road.tile_map[0]:
-                hor_x_end += hor_dx
-                hor_y_end += hor_dy
-                pg.draw.circle(self.game.screen,
-                'white',
-                (hor_x_end, hor_y_end),
-                5)
-
-
-            #vertical raycasting
-            ver_dx = TILE_SIZE
-            ver_dy = TILE_SIZE / (math.tan(self.ray_angle(x)) + 1e-10)
-            ver_x_end = self.tile_pos[0]
-            ver_y_end = self.tile_pos[1]
-
-            for _ in self.road.tile_map:
-                ver_x_end += ver_dx
-                ver_y_end += ver_dy
-                pg.draw.circle(self.game.screen,
-                'white',
-                (ver_x_end, ver_y_end),
-                5)
-    
-    
-            self.ray_ends[x] = (self.start_point[0] + math.sin(self.ray_angle(x)) * -100, self.start_point[1] + math.cos(self.ray_angle(x)) * -100)
-
     def draw(self):
         for end_point in self.ray_ends:
             pg.draw.line(
                 self.game.screen,
                 'white',
-                (self.start_point[0], self.start_point[1]),
+                self.car.center,
                 (end_point[0], end_point[1]),
                 3)
             pg.draw.circle(self.game.screen,
                 'orange',
                 (end_point[0], end_point[1]),
-                4)
+                4)    
             
     def update(self):
         self.cast_rays()
